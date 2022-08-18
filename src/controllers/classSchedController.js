c9e2e598447f251e9790a24858208a8479c4cd81
kcirtappat <noscoj.pat@gmail.com>
2022-08-18 11:08:22 +0800

    added logic

--- src/controllers/classSchedController.js
+++ src/controllers/classSchedController.js
@@ -3,20 +3,28 @@ const mongoose = require('mongoose');
 
 // Get All Class Schedule
 const getClassSched = async (req, res) => {
-    try {
-        // logic
-    } catch (error) {
-        // error handler
-    }
+    const classSched = await ClassSchedule.find({}).sort({ createdAt: -1 })
+
+    res.status(200).json(classSched)
 };
 
 // Update Class Schedule
 const updateClassSched = async (req, res) => {
-    try {
-        // logic
-    } catch (error) {
-        // error handler
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such workout' })
     }
+
+    const classSched = await ClassSchedule.findOneAndUpdate({ _id: id }, {
+        ...req.body
+    })
+
+    if (!classSched) {
+        return res.status(400).json({ error: 'No such workout' })
+    }
+
+    res.status(200).json(classSched)
 };
 
 module.exports = {
